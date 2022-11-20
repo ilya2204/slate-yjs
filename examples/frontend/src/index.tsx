eecfb63bebd89285b897b3f524b735bfcc08616a
ilya <ilya-z2204@mail.ru>
2022-11-20 19:29:11 +0300

    update react

--- examples/frontend/src/index.tsx
+++ examples/frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import 'virtual:windi.css';
 import 'virtual:windi-devtools';
@@ -9,7 +9,11 @@ import { RemoteCursorsOverlayPage } from './pages/RemoteCursorOverlay';
 import { SimplePage } from './pages/Simple';
 import { RemoteCursorDecorations } from './pages/RemoteCursorDecorations';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
@@ -27,6 +31,5 @@ ReactDOM.render(
       </Routes>
       <Navigator />
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
